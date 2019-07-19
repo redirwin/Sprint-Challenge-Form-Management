954ca8bb2b05eef2bd8d2132175b5db8dc03478c
Dave Irwin <david@redirwin.com>
2019-07-19 08:46:14 -0700

    Form created and accepting values.

--- client/src/components/RegistrationForm.js
+++ client/src/components/RegistrationForm.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-function RegistrationForm() {
-  <div />;
+function RegistrationForm({ touched, errors }) {
+  return (
+    <div className="form-wrapper">
+      <Form>
+        <div className="form-group">
+          <Field
+            className="input"
+            name="userName"
+            type="text"
+            placeholder="username"
+            autoComplete="off"
+          />
+          <p className="error-message">{touched.userName && errors.userName}</p>
+        </div>
+        <div className="form-group">
+          <Field
+            className="input"
+            name="password"
+            type="password"
+            placeholder="Password"
+            autoComplete="off"
+          />
+          <p className="error-message">{touched.password && errors.password}</p>
+        </div>
+        <button>Submit</button>
+      </Form>
+    </div>
+  );
 }
+export default withFormik({
+  mapPropsToValues() {
+    return {
+      userName: "",
+      password: ""
+    };
+  },
 
-export default RegistrationForm;
+  validationSchema: Yup.object().shape({
+    userName: Yup.string()
+      .required("Username is required.")
+      .min(8, "Username must bo at least 8 characters."),
+    password: Yup.string()
+      .required("Password is required.")
+      .min(8, "Password must bo at least 8 characters.")
+  }),
+
+  handleSubmit(values, formikBag) {
+    console.log(values);
+    //   axiosWithAuth()
+    //     .post("/api/friends", values)
+    //     .then(res => {
+    //       formikBag.props.updateFriends(res.data);
+    //       formikBag.resetForm();
+    //       formikBag.setSubmitting();
+    //     })
+    //     .catch(err => {
+    //       console.log(err);
+    //     });
+  }
+})(RegistrationForm);
--- client/src/components/ShowData.js
+++ client/src/components/ShowData.js
@@ -1,7 +1,5 @@
-import React from "react"
+import React from "react";
 
 export default function ShowData() {
-    <div></div>
+  return <div />;
 }
-
-export default ShowData
